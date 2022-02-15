8ec2561832e7ee761b3ddc0aa8cb1b154f51b54e
Juan Esteban <juanes0202@gmail.com>
2022-02-15 08:47:56 -0500

    uninstall day.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import dayjs from "dayjs";
 
 import "./assets/css/tailwind.css";
 
-createApp(App).use(store).use(router).use(dayjs).mount("#app");
+createApp(App).use(store).use(router).mount("#app");
